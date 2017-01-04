f1e66c04c55a0e1786fcc81b1df555ba6a723de9
Sebastien Huet <sebhuet@gmail.com>
2017-01-04 14:49:09 +0100

    IR OK!  let's clean code...

--- app/src/main/java/fr/sebhuet/androidthings/ir_receiver/MainActivity.java
+++ app/src/main/java/fr/sebhuet/androidthings/ir_receiver/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends Activity {
     private static final String TAG = fr.sebhuet.androidthings.ir_receiver.MainActivity.class.getSimpleName();
 
     private Gpio mButtonGpio;
+    private Gpio mIRReceiverGpio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,21 +25,41 @@ public class MainActivity extends Activity {
 
         PeripheralManagerService service = new PeripheralManagerService();
         try {
-            String pinName = BoardDefaults.getGPIOForButton();
-            mButtonGpio = service.openGpio(pinName);
-            mButtonGpio.setDirection(Gpio.DIRECTION_IN);
-            mButtonGpio.setEdgeTriggerType(Gpio.EDGE_FALLING);
-            mButtonGpio.registerGpioCallback(new GpioCallback() {
-                @Override
-                public boolean onGpioEdge(Gpio gpio) {
-                    Log.i(TAG, "GPIO changed, button pressed");
-                    // Return true to continue listening to events
-                    return true;
-                }
-            });
+            glueButton(service);
+            glueIRReceiver(service);
         } catch (IOException e) {
             Log.e(TAG, "Error on PeripheralIO API", e);
         }
+    }
+
+    private void glueButton(PeripheralManagerService service) throws IOException {
+        String pinName = BoardDefaults.getGPIOForButton();
+        mButtonGpio = service.openGpio(pinName);
+        mButtonGpio.setDirection(Gpio.DIRECTION_IN);
+        mButtonGpio.setEdgeTriggerType(Gpio.EDGE_FALLING);
+        mButtonGpio.registerGpioCallback(new GpioCallback() {
+            @Override
+            public boolean onGpioEdge(Gpio gpio) {
+                Log.i(TAG, "GPIO changed, button pressed");
+                // Return true to continue listening to events
+                return true;
+            }
+        });
+    }
+
+    private void glueIRReceiver(PeripheralManagerService service) throws IOException {
+        String pinName = BoardDefaults.getGPIOForIrReceiver();
+        mIRReceiverGpio = service.openGpio(pinName);
+        mIRReceiverGpio.setDirection(Gpio.DIRECTION_IN);
+        mIRReceiverGpio.setEdgeTriggerType(Gpio.EDGE_FALLING);
+        mIRReceiverGpio.registerGpioCallback(new GpioCallback() {
+            @Override
+            public boolean onGpioEdge(Gpio gpio) {
+                Log.i(TAG, "GPIO changed, IR Received pressed");
+                // Return true to continue listening to events
+                return true;
+            }
+        });
     }
 
     @Override
